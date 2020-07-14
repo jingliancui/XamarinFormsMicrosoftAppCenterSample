06b2cadd906b77ffa393d154820d40710899dd50
jingliancui <ericcui@qq.com>
2020-07-14 14:43:30 +0800

    add sample code
    add sample code

---
+++ SampleApp/SampleApp/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace SampleApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            //使用分号隔开多个key
+            AppCenter.Start("ios=ios的secret;" +
+                     "android=android的secret",
+                     typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
